06c8f016410d484b10deb4c279ff97e51f037e2d
Varun <imnotvarun@gmail.com>
2024-03-06 12:47:09 +0000

    added comments for better telling of the code

--- home/index.js
+++ home/index.js
@@ -1,16 +1,3 @@
-// Function to trigger the fade-out animation and navigate to the home page
-function fadeOutAndNavigate() {
-    // Add a class to the body to initiate the fade-out animation
-    document.body.classList.add('fade-out-animation');
-
-    // After a delay (matching the animation duration), navigate to the home page
-    setTimeout(function() {
-        window.location.href = 'index.html'; // Replace 'index.html' with the actual URL of your home page
-    }, 1000); // Adjust the delay as needed to match the duration of your animation
-}
-
-// Get reference to the circle elements
-const circles = document.querySelectorAll('.circle-left, .circle-left2, .circle-left3, .circle-left4');
 
 // Function to handle scroll event and trigger animations
 function handleScroll() {
